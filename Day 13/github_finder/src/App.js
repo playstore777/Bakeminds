1c7027154dc241e739b35405d4e1ef27f570ff79
playstore777 <67255734+playstore777@users.noreply.github.com>
2021-09-27 23:05:27 +0530

    worked on github finder and completed bankbot assignment

--- Day 13/github_finder/src/App.js
+++ Day 13/github_finder/src/App.js
@@ -2,35 +2,90 @@ import axios from "axios";
 import React, { Component } from "react";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
+import ShowAlert from "./components/layout/ShowAlert";
+import Search from "./components/user/Search";
 import Users from "./components/user/Users";
 
 class App extends Component {
   state = {
     users: [],
     loading: false,
+    alert: null,
   };
 
-  async componentDidMount() {
-    this.setState({
-      loading: true,
-    });
+  // async componentDidMount() {
+  //   this.setState({
+  //     loading: true,
+  //   });
 
-    let res = await axios.get("https://api.github.com/users");
-    // console.log("res.data : ", res, "loading: ", this.state.loading);
+  //   let res = await axios.get("https://api.github.com/users");
+  //   // console.log("res.data : ", res, "loading: ", this.state.loading);
 
+  //   this.setState({
+  //     users: res.data,
+  //     loading: false,
+  //   });
+  //   // console.log("users : ", this.state.users, "loading: ", this.state.loading);
+  // }
+
+  showAlert = (msg, type) => {
     this.setState({
-      users: res.data,
       loading: false,
+      alert: { msg, type },
     });
-    // console.log("users : ", this.state.users, "loading: ", this.state.loading);
-  }
+    console.log(this.state.loading);
+
+    setTimeout(
+      () =>
+        this.setState({
+          alert: null,
+          loading: false,
+        }),
+      // console.log("5 secs done!"),
+      5000
+    );
+  };
+
+  onSubmit = async (value) => {
+    if (this.state.alert === null) {
+      console.log("e");
+
+      if (value === "") {
+        this.showAlert("Please enter something", "light");
+        return;
+      }
+
+      // e.preventDefault();
+
+      this.setState({
+        loading: true,
+      });
+
+      console.log(value);
+      let res = await axios.get(
+        `https://api.github.com/search/users?q=${value}&client_id=${process.env.REACT_APP_CLIENT_ID}&client_secret=${process.env.REACT_APP_CLIENT_SECRET}`
+      );
+      console.log("res.data : ", res.data, "loading: ", this.state.loading);
+
+      this.setState({
+        users: res.data,
+        loading: false,
+      });
+    }
+  };
+
+  clear = () => this.setState({ users: [] });
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <Navbar icon="fab fa-github" title="Github Finder" />
-          <Users data={this.state.users} />
+          {this.state.alert !== null ? (
+            <ShowAlert alert={this.state.alert} />
+          ) : null}
+          <Search onSubmit={this.onSubmit} clear={this.clear} />
+          <Users data={this.state.users} loading={this.state.loading} />
         </header>
       </div>
     );
--- Day 13/github_finder/src/components/user/Users.js
+++ Day 13/github_finder/src/components/user/Users.js
@@ -1,16 +1,25 @@
 import UserItem from "./UserItem";
+import Spinner from "../layout/Spinner";
 
 const Users = (props) => {
   const style = {
     display: "grid",
     gridTemplateColumns: "repeat(3, 1fr)",
+    gridGaps: "1rem",
   };
 
-  const usersList = props.data.map((user) => {
-    console.log("user : ", user);
-    return <UserItem key={user.id} data={user} />;
-  });
-  return (
+  console.log("props.data : ", props.data);
+  let usersList = [];
+  if (props.data.items) {
+    usersList = props.data.items.map((user) => {
+      console.log("user : ", user);
+      return <UserItem key={user.id} data={user} />;
+    });
+  }
+  // console.log(usersList);
+  return props.loading ? (
+    <Spinner />
+  ) : (
     <div>
       <div style={style}>{usersList}</div>
     </div>
