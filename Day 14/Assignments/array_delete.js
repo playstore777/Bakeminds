acc56197dba021ef53a56da0bc42dcdfec0751e6
playstore777 <67255734+playstore777@users.noreply.github.com>
2021-09-27 07:45:38 +0530

    fixed the code

--- Day 14/Assignments/array_delete.js
+++ Day 14/Assignments/array_delete.js
@@ -1,9 +1,12 @@
 Array.prototype.delete = function (value) {
   let index = this.indexOf(value);
-  if (index === -1) this.splice(index, 1);
+  if (index !== -1) {
+    this.splice(index, 1);
+  }
   return this;
 };
 
+console.log([8, 3, 7, 4].delete(9));
 console.log([8, 3, 7, 4].delete(7));
 console.log(["hello", "z", 41, "well"].delete("z"));
 console.log([7, 2, 9, 5, 3].delete(2));
